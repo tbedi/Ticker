539b809f4e1722f658dfb80523413bfaca076f3c
Avinash <apatil@kraususa.com>
2014-02-25 16:19:02 +0530

    Fist Commit For ticker portal

---
+++ Ticker/Forms/MasterForms/mstMain.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ticker.Forms.MasterForms
+{
+    public partial class mstMain : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
