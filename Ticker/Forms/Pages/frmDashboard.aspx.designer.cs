49436e459e02c5db6ff5da7c8790ce7f561435a6
Shriram Rajaram <srajararam@kraususa.com>
2014-03-04 10:54:53 -0500

    Changes

--- Ticker/Forms/Pages/frmDashboard.aspx.designer.cs
+++ Ticker/Forms/Pages/frmDashboard.aspx.designer.cs
@@ -193,6 +193,15 @@ namespace Ticker.Forms.Pages {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
+        /// Label9 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label9;
+        
+        /// <summary>
         /// UpdatePanel10 control.
         /// </summary>
         /// <remarks>
@@ -254,6 +263,15 @@ namespace Ticker.Forms.Pages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Label10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label10;
         
         /// <summary>
         /// UpdatePanel11 control.
